4497f6045fd0c873a99bb92206e71fd9286b3d75
Yabgonz0310 <yabingonzaga0310@gmail.com>
2025-04-22 19:45:32 -0600

    Actualización de busqueda de canciones en youtube

--- steps/loginSteps.js
+++ steps/loginSteps.js
@@ -3,27 +3,18 @@ import LoginPage from '../pages/loginPage.js';
 const { I } = inject();
 
 Given("Estoy en la pagina principal", () => {
-  /*I.amOnPage("https://www.inmotionhosting.com/");
-  I.wait(10);*/
-  LoginPage.iniciopag();
+    LoginPage.iniciopag();
 });
 
 When("Voy a la pagina de login", () => {
-// I.waitForElement('//a[@aria-label="Login"]',2);
-  //I.click('//a[text()="Login"]');
 LoginPage.clickLoginButton();
 });
 
 When("Escribo nombre de usuario y contrasena", () => {
-  /*I.wait(5);
-  I.fillField('//input [@placeholder="email address"]', "yabingonzaga0310@gmail.com");
-  I.fillField('//input [@placeholder="password"]', "Ejemplo0920#");
-  I.wait(2) */
-  LoginPage.ingcredenciales();
+   LoginPage.ingcredenciales();
 });
 
 Then("Se visualiza link de ayuda", () => {
-  //I.seeElement('//a[text()="Need help logging in?"]', 10);
-  LoginPage.verBtnAyuda();
+   LoginPage.verBtnAyuda();
 });
 
